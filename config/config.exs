49b101a5a0f80f7f8126fceb9ac8b48a730a732e
Chad Gorshing <gorshing@gmail.com>
2017-10-26 22:02:57 -0500

    Removed importing env's (bad copy/paste I think)

--- config/config.exs
+++ config/config.exs
@@ -11,7 +11,3 @@ if Mix.env == :test do
     client_id: System.get_env("KHANACADEMY_CONSUMER_KEY"),
     client_secret: System.get_env("KHANACADEMY_CONSUMER_SECRET")
 end
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
