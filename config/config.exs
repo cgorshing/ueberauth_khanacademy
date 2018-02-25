119159a619589f66df9606ec240799e0edb0a62c
Chad Gorshing <gorshing@gmail.com>
2018-02-25 09:25:51 -0600

    Moved to env specific files

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,3 @@
 use Mix.Config
 
-if Mix.env == :test do
-
-  config :ueberauth, Ueberauth,
-    providers: [
-      khanacademy: {Ueberauth.Strategy.KhanAcademy, []}
-    ]
-
-  config :ueberauth, Ueberauth.Strategy.KhanAcademy.OAuth,
-    client_id: System.get_env("KHANACADEMY_CONSUMER_KEY"),
-    client_secret: System.get_env("KHANACADEMY_CONSUMER_SECRET")
-end
+import_config "#{Mix.env}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,12 @@
 use Mix.Config
 
 config :tesla, adapter: :mock
+
+config :ueberauth, Ueberauth,
+  providers: [
+    khanacademy: {Ueberauth.Strategy.KhanAcademy, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.KhanAcademy.OAuth,
+  consumer_key: "FAKE_CONSUMER_KEY",
+  consumer_secret: "FAKE_CONSUMER_SECRET"
